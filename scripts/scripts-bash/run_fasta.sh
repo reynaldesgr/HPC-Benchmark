6fbfc53950413287df7e42ce76d797ebe1d5a5a9
reynaldesgr <reynalde.segerie.deveze@gmail.com>
2024-02-09 13:59:48 +0100

    Fix fasta for Erlang

--- scripts/scripts-bash/run_fasta.sh
+++ scripts/scripts-bash/run_fasta.sh
@@ -4,28 +4,28 @@ base_path="../../"
 
 # FASTA
 
-#cd "${base_path}c/fasta"
-#gcc fasta.c -o fasta
-#echo "[Fasta] - run C file..."
-#time ./fasta 25000000
-#cd - > /dev/null
+cd "${base_path}c/fasta"
+gcc fasta.c -o fasta
+echo "[Fasta] - run C file..."
+time ./fasta 25000000
+cd - > /dev/null
 
 ################################################
 
-#cd "${base_path}erlang/fasta"
-#erlc fasta.erl
-#echo "[Binary trees] - run Erlang file..."
-#time erl -noshell -s fasta main 25000000 -s init stop
-#cd - > /dev/null
-#echo -e "\n\n"
+cd "${base_path}erlang/fasta"
+erlc fasta.erl
+echo "[Binary trees] - run Erlang file..."
+time erl -noshell -run fasta main 25000000
+cd - > /dev/null
+echo -e "\n\n"
 
 ################################################
 
 
-#cd "${base_path}python/fasta"
-#echo "[Fasta] - run Python file..."
-#time python3 fasta.py 25000000
-#cd - > /dev/null
+cd "${base_path}python/fasta"
+echo "[Fasta] - run Python file..."
+time python3 fasta.py 25000000
+cd - > /dev/null
 
 #################################################
 
