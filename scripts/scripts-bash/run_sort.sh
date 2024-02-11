4b85e19a60ee28088550c454e6bebd239d716256
reynaldesgr <reynalde.segerie.deveze@gmail.com>
2024-02-11 09:11:34 +0100

    Fix bug

--- scripts/scripts-bash/run_sort.sh
+++ scripts/scripts-bash/run_sort.sh
@@ -3,14 +3,14 @@
 base_path="../../"
 
 cd "${base_path}c/sort"
-gcc sort.c -o sort_c
+gcc bubble-sort.c -o sort_c
 echo "[Bubble sort] - run C file..."
 time ./sort_c
 cd - > /dev/null
 echo -e "\n\n"
 
 cd "${base_path}cpp/sort"
-g++ sort.cpp -o sort_cpp
+g++ bubble-sort.cpp -o sort_cpp
 echo "[Bubble sort] - run C++ file..."
 time ./sort_cpp
 cd - > /dev/null
@@ -24,7 +24,7 @@ cd - > /dev/null
 echo -e "\n\n"
 
 cd "${base_path}fortran/sort"
-gfortran -o sort_fortran -ffree-line-length-none -fmax-array-constructor=1000000 ./*.f90
+gfortran -o bubble-sort_fortran -ffree-line-length-none -fmax-array-constructor=1000000 ./*.f90
 echo "[Bubble sort] - run Fortran file..."
 time ./sort_fortran
 cd - > /dev/null
@@ -39,18 +39,18 @@ echo -e "\n\n"
 
 cd "${base_path}python/sort"
 echo "[Bubble sort] - run Python file..."
-time python3 sort.py
+time python3 bubble-sort.py
 cd - > /dev/null
 echo -e "\n\n"
 
 cd "${base_path}ruby/sort"
 echo "[Bubble sort] - run Ruby file..."
-time ruby sort.rb
+time ruby bubble-sort.rb
 cd - > /dev/null
 echo -e "\n\n"
 
 cd "${base_path}rust/sort"
-rustc sort.rs -o sort_rust
+rustc bubble-sort.rs -o sort_rust
 echo "[Bubble sort] - run Rust file..."
 time ./sort_rust
 cd - > /dev/null
