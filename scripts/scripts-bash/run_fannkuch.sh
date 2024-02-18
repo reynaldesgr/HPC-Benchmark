109a167e7a86ded4cb8486dd73193539455c77db
reynaldesgr <reynalde.segerie.deveze@gmail.com>
2024-02-18 11:05:58 +0100

    Add fannkuch benchmarks for Ruby, Java, Fortran & C++

--- scripts/scripts-bash/run_fannkuch.sh
+++ scripts/scripts-bash/run_fannkuch.sh
@@ -2,15 +2,22 @@
 
 base_path="../../"
 
+
 # FANNKUCH
 
 cd "${base_path}c/fannkuch-redux"
-gcc fannkuch-redux.c -o fannkuch-redux
+gcc fannkuchredux.c -o fannkuch-redux_c
 echo "[Fannkuch redux] - run C file..."
-time ./fannkuch-redux
+time ./fannkuch-redux_c
 cd - > /dev/null
+echo -e "\n\n"
 
-################################################
+cd "${base_path}c++/fannkuch-redux"
+g++ fannkuchredux.cpp -o fannkuch-redux_cpp
+echo "[Fannkuch redux] - run C++ file..."
+time ./fannkuch-redux_cpp
+cd - > /dev/null
+echo -e "\n\n"
 
 cd "${base_path}erlang/fannkuch-redux"
 erlc fannkuch.erl
@@ -19,19 +26,34 @@ time erl -noshell -run fannkuch main 12
 cd - > /dev/null
 echo -e "\n\n"
 
-################################################
+cd "${base_path}fortran/fannkuch-redux"
+gfortran -o fannkuch-redux_fortran ./*.f90
+echo "[Fannkuch redux] - run Fortran file..."
+time ./fannkuch-redux_fortran 12
+cd - > /dev/null
+echo -e "\n\n"
+
+cd "${base_path}java/fannkuch-redux"
+javac -d bin -sourcepath src src/fannkuchredux.java
+echo "[Fannkuch redux] - run Java file..."
+time java -cp bin fannkuchredux 12
+cd - > /dev/null
+echo -e "\n\n"
+
 cd "${base_path}python/fannkuch-redux"
 echo "[Fannkuch redux] - run Python file..."
-time python3 fannkuch-redux.py
+time python3 fannkuchredux.py
 cd - > /dev/null
+echo -e "\n\n"
 
-#################################################
+cd "${base_path}ruby/fannkuch-redux"
+echo "[Fannkuch redux] - run Ruby file..."
+time ruby fannkuchredux.rb 12
+cd - > /dev/null
+echo -e "\n\n"
 
 cd "${base_path}rust/fannkuch-redux"
-rustc fannkuch-redux.rs -o fannkuch-redux
+rustc fannkuchredux.rs -o fannkuch-redux_rust
 echo "[Fannkuch redux] - run Rust file..."
-time ./fannkuch-redux 
+time ./fannkuch-redux_rust
 cd - > /dev/null
-
-#################################################
-
